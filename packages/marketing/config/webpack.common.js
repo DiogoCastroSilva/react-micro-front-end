c09889cb6b44aebfa2eabe0f9d2f15b77805cc48
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-05 15:10:48 +0000

    Update webpack

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     module: {
@@ -14,5 +15,10 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 };
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common');
@@ -22,9 +21,6 @@ const devConfig = {
                 './App': './src/bootstrap'
             },
             shared: packageConfig.dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
         })
     ]
 };
