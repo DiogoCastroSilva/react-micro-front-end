4c28437f72bf7878c20e81c6c16c8fcfee5f921e
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-09 09:32:03 +0000

    Add loading components and lazy loading to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Components
 import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
+import Progress from './components/Progress';
+
+// Lazy components
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 
 export default () => {
@@ -12,11 +15,12 @@ export default () => {
         <BrowserRouter>
             <div>
                 <Header />
-                <Switch>
-                    <Route path='/auth' component={AuthApp} />
-                    <Route path='/' component={MarketingApp} />
-                </Switch>
-                <MarketingApp />
+                <Suspense fallback={<Progress />}>
+                    <Switch>
+                        <Route path='/auth' component={AuthLazy} />
+                        <Route path='/' component={MarketingLazy} />
+                    </Switch>
+                </Suspense>
             </div>
         </BrowserRouter>
     );
---
+++ packages/container/src/components/Progress.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => {
+    return createStyles({
+        bar: {
+            width: '100%',
+            '& > * + *': {
+                marginTop: theme.spacing(2)
+            }
+        }
+    });
+});
+
+export default () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.bar}>
+            <LinearProgress />
+        </div>
+    );
+};
